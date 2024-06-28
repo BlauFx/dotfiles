19f01fc9104540056484df2146cc8294815f9943
BlauFx <blaufxsup@gmail.com>
2024-06-28 20:46:44 +0200

    Add safety check
    Even tho it has no impact, why not

--- scripts/totp.sh
+++ scripts/totp.sh
@@ -19,10 +19,17 @@ while IFS="|" read key name
 do
     echo $num: $name
     num=$((num+1))
+
     listofkeys+=($key)
 done < $keysFile
 
 read -p "Which one?: " userInput
+numLength=$(($num-1));
+
+if [[ ! $userInput =~ ^[0-9]*$ ]] || [ "$userInput" -gt "$numLength" ]; then
+    echo -e "\nPlease enter a number between 1-$numLength."
+    exit
+fi
 
 #decrement userInput by one because for the user, one is the first element not zero.
 userInput=$((userInput-1))
