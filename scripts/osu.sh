e27706f1f0f56ac36ab28d7b44fc7f17b27f453d
BlauFx <blaufxsup@gmail.com>
2024-08-05 22:29:08 +0200

    Change osu.sh

--- scripts/osu.sh
+++ scripts/osu.sh
@@ -5,21 +5,21 @@ fileDev="$HOME"/.local/share/osu-development
 
 drive=/run/media/"$USER"/General/Games
 
+# Link osu-stable to home folder so lazer can access stable files
 if [ ! -L $file ]
 then
 	ln -s "$drive"/osu/drive_c/osu/ $file
 fi
 
+# Store osu-lazer development files in $drive/osu-development not in home folder
 if [ ! -L $fileDev ]
 then
 	ln -s "$drive"/osu-development/ $fileDev
 fi
 
-changeLazerStorage() {
-	localosufolder=$HOME/.local/share/osu
-	osulazerGamefiles="$drive"/osulazer
+# Change lazer folder location
+localosufolder=$HOME/.local/share/osu
+osulazerGamefiles="$drive"/osulazer
 	
-	mkdir -p $localosufolder
-	echo "FullPath = $osulazerGamefiles " > $localosufolder/storage.ini
-}
-
+mkdir -p $localosufolder
+echo "FullPath = $osulazerGamefiles " > $localosufolder/storage.ini
